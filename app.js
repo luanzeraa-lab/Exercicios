04582543ce46887fbbbd45c0c98aa0541e8ac5b7
Luan Silva de Picoli <luansilva_2@hotmail.com>
2025-08-28 22:47:22 -0300

    so falta deletar cursos

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ let alunos = [
 
      //-----------ALTERAR ALUNO
      app.put('/alunos', (req,res) =>{
+      const {ra} = req.query;
+      const {name, turma} = req.body;
         const index = alunos.findIndex(aluno => aluno.ra == req.query.ra)
-        alunos[index] = {name: req.body.name, turma: req.body.turma}
+        alunos[index] = {ra : ra, name: req.body.name, turma: req.body.turma}
         res.send(JSON.stringify(alunos));
      })
 
@@ -55,15 +57,38 @@ let alunos = [
 
      //-----------DELETAR ALUNO
      app.delete('/alunos', (req, res) =>{
+      const {ra} = req.query;
         const index = alunos.findIndex(aluno => aluno.ra == req.query.ra)
         alunos.splice(index, 1)
         res.send(JSON.stringify(alunos))
      })
 
      //----------DELETAR CURSO
-     app.delete('/alunos/curso')
+   app.delete('/alunos/cursos/cursosdeletar', (req, res) =>{
+         const {ra} = req.query;
+         const cursoDeletado = req.body(cursos);
+         res.send(cursoDeletado)
+         console.log(cursoDeletado);
+         const index = alunos.findIndex(aluno => aluno.ra == ra)
+   
+         for (i = 0; i < alunos[index].cursos.length; i++){
+            if (alunos[index].cursos[i] == cursoDeletado){
+               alunos[index].cursos.splice(i, 1)
+            }
+         }
+         res.send(JSON.stringify(alunos))
+      });
 
-
+      //--------------MOSTRAR ALUNOS---okk
+     app.get('/alunos', (req,res) =>{
+      res.send(alunos);
+     })
+     //-------------MOSTRA ALUNOS PELO RA----ok
+     app.get('/alunos/alunora', (req, res) =>{
+      const { ra } = req.query;
+      const index = alunos.findIndex(x => x.ra == ra);
+      res.send(alunos[index]);
+     })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
